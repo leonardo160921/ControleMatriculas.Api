db21b0c2ab86bff59ca3571be7dce4abd4f1264c
Leonardo Souza <lleonardo.souzza123@gmail.com>
2026-09-04 11:24:08 -0300

    Turma Listar configurado, semelhante ao aluno

---
+++ ControleMatriculas.Api/Controllers/TurmasController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using ControleMatriculas.Api.Repositories;
+
+namespace ControleMatriculas.Api.Controllers
+{
+    [RoutePrefix("api/turmas")]
+    public class TurmasController : ApiController
+    {
+        private readonly ITurmaRepository _turmaRepository;
+
+        public TurmasController(ITurmaRepository turmaRepository)
+        {
+            _turmaRepository = turmaRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult Listar()
+        {
+            var turmas = _turmaRepository.Listar();
+
+            return Ok(turmas);
+        }
+    }
+}
---
+++ ControleMatriculas.Api/Repositories/TurmaRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using ControleMatriculas.Api.Models;
+using System.Collections.Generic;
+
+namespace ControleMatriculas.Api.Repositories
+{
+    public class TurmaRepository : ITurmaRepository
+    {
+        private readonly string _connectionString;
+
+        public TurmaRepository()
+        {
+            _connectionString =
+                ConfigurationManager
+                    .ConnectionStrings["TesteEscola"]
+                    .ConnectionString;
+        }
+
+        public IEnumerable<Turma> Listar()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Turma>(
+                    "dbo.usp_Turma_Listar",
+                    commandType: CommandType.StoredProcedure
+                ).ToList();
+            }
+        }
+    }
+}
